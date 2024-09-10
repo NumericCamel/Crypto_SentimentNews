7075a41d27f8eca7a976eb24bdf5b11bbd3fcd71
NumericCamel <erenmuller@icloud.com>
2024-09-10 21:43:30 +0400

    Changes

--- secret.py
+++ secret.py
@@ -1,8 +1,4 @@
 
 
 secrets = {
-    'client_id' : '8MAahEPJBSixOfm-fQ8yPw',
-    'client_secret' : 'e3hY0q_c-i1ZCxn8yF-m-iBC3_tc-w',
-    'username' : 'CamelQuant',
-    'password' : 'GoatedMillion619!'
 }
